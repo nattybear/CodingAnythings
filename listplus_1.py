c47bbc9caf600ca6d3265ce27ae82ea0c469c05f
PARK JOON GYU <nattybear@naver.com>
2018-12-09 17:13:20 +0900

    Add the function that using built-in map function

--- listplus_1.py
+++ listplus_1.py
@@ -13,6 +13,10 @@ def pythonic(xs):
 def list_comp(xs):
     return [x + 1 for x in xs]
 
+def map_inc(xs):
+    def inc(x): return x + 1
+    return list(map(inc, xs))
+
 makeA = input().split()
 makeA = list(map(int, makeA))
 
