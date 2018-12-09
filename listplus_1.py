09f6983104d22d655856798805ca17581149876d
PARK JOON GYU <nattybear@naver.com>
2018-12-09 18:28:51 +0900

    Change inc() function to the anonymous function

--- listplus_1.py
+++ listplus_1.py
@@ -14,8 +14,7 @@ def list_comp(xs):
     return [x + 1 for x in xs]
 
 def map_inc(xs):
-    def inc(x): return x + 1
-    return list(map(inc, xs))
+    return list(map(lambda x: x + 1, xs))
 
 makeA = input().split()
 makeA = list(map(int, makeA))
